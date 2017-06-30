f314ac7195dc8e9ee582d90f8335fde787cd9963
Zhang Li <tornadory@gmail.com>
2017-06-30 11:21:23 +0800

    change index.html, add argon.js demo pages
    change index.html, add argon.js demo pages

---
+++ argon/app.js
@@ -0,0 +1,29 @@
+// initialize Argon
+var app = Argon.init();
+app.context.setDefaultReferenceFrame(app.context.localOriginEastUpSouth);
+
+// initialize THREE
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera();
+var userLocation = new THREE.Object3D;
+scene.add(camera);
+scene.add(userLocation);
+
+var boxGeoObject = new THREE.Object3D();
+var box = new THREE.Object3D();
+var loader = new THREE.TextureLoader();
+loader.load( "box.png", function ( texture ) {
+    var geometry = new THREE.BoxGeometry(2, 2, 2);
+    var material = new THREE.MeshBasicMaterial( { map: texture } );
+    var mesh = new THREE.Mesh( geometry, material );
+    box.add( mesh );
+});
+boxGeoObject.add(box);
+scene.add(boxGeoObject);
+
+var renderer = new THREE.WebGLRenderer({
+    alpha: true,
+    logarithmicDepthBuffer: true
+});
+renderer.setPixelRatio(window.devicePixelRatio);
+app.view.element.appendChild(renderer.domElement);
