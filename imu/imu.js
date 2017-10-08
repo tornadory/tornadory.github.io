c17cd448ebf3101930afa888baf7575aac7a1436
AliZhang <tornadory@gmail.com>
2017-10-08 21:01:53 +0800

    add imu test

--- imu/imu.js
+++ imu/imu.js
@@ -1,16 +1,19 @@
 var imudatainfo = $('#imudata');
 
 if (window.DeviceOrientationEvent) {
+  console.log("window.DeviceOrientationEvent supported");
     window.addEventListener("deviceorientation", function () {
         imudatainfo.innerHTML = event.beta + " - " + event.gamma;
         // tilt([event.beta, event.gamma]);
     }, true);
 } else if (window.DeviceMotionEvent) {
+  console.log("window.DeviceMotionEvent supported");
     window.addEventListener('devicemotion', function () {
         // tilt([event.acceleration.x * 2, event.acceleration.y * 2]);
         imudatainfo.innerHTML = event.acceleration.x * 2 + " - " + event.acceleration.y * 2;
     }, true);
 } else {
+    console.log("no gyro supported");
     imudatainfo.innerHTML = "no gyro support";
     // window.addEventListener("MozOrientation", function () {
     //     // tilt([orientation.x * 50, orientation.y * 50]);
